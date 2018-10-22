7deb77899d0dc26dbc8ce66eb2b1d23ff7faf5b3
Jari Vetoniemi <mailroxas@gmail.com>
2018-10-22 18:29:12 +0300

    Remove recommendations as it's safety hazard

--- src/ptrace-address-rw.c
+++ src/ptrace-address-rw.c
@@ -3,7 +3,6 @@
 
 // This address-rw uses ptrace
 // This works with older kernels, but it also ensures non racy read/writes, as it stops the process.
-// It's recommended to `setcap cap_sys_ptrace=eip ptrace-region-rw` to run this tool without sudo
 
 int
 main(int argc, const char *argv[])
--- src/uio-address-rw.c
+++ src/uio-address-rw.c
@@ -4,7 +4,6 @@
 // This address-rw uses uio
 // It needs recent kernel, but may be racy as it reads / writes while process is running.
 // Ideal for realtime memory tools, however uio can't write non-writable memory(!).
-// It's recommended to `setcap cap_sys_ptrace=eip uio-region-rw` to run this tool without sudo
 
 int
 main(int argc, const char *argv[])
